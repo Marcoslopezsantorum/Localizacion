cf52a1be8cfecd574d7ae44f42eaf2667f2564d2
Marcos López Santorum <mlopezsantorum@danielcastelao.org>
2021-12-05 21:58:37 +0100

    fin localizacion

--- app/src/main/java/com/example/localizacion/MainActivity.kt
+++ app/src/main/java/com/example/localizacion/MainActivity.kt
@@ -1,11 +1,165 @@
 package com.example.localizacion
 
+
+import android.Manifest
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.OperationApplicationException
 
-class MainActivity : AppCompatActivity() {
+import android.provider.ContactsContract
+
+import android.content.ContentResolver
+
+import android.content.ContentProviderOperation
+import android.content.pm.PackageManager
+import android.widget.Toast
+import android.content.DialogInterface
+import android.location.Location
+import android.os.RemoteException
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Transformations.map
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback,
+    GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+    private lateinit var map: GoogleMap
+
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationClickListener(this)
+        enableMyLocation()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createMapFragment()
+    }
+
+    // Implementa el fragment del mapa
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    // Sirve pra saber si se ha dado el permiso
+    private fun isPermissionsGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+    // Comprueba si el mapa se ha iniciado y los permisos de la siguiente función
+    private fun enableMyLocation() {
+        if (!::map.isInitialized) return
+        if (isPermissionsGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                return
+            }
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    // La siguiente funcion que se ejecuta despues de la mencionada antes
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION
+            )
+        }
+    }
+    // Esto nos dice si acepta los permisos que le dimos
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    return
+                }
+                map.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(
+                    this,
+                    "Para activar la localización acepta los permisos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else -> {}
+        }
+    }
+    //Esto es para que no se borren los permisos al salir de la aplicación
+    // comprueba que el mapa está cargado y si lo está comprueba que los permisos esten activos
+    // si no lo están desactiva la localización en tiempo real
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::map.isInitialized) return
+        if (!isPermissionsGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                return
+            }
+            map.isMyLocationEnabled = false
+            Toast.makeText(
+                this,
+                "Para activar la localización  acepta los permisos",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+    //Esto centra el mapa donde estas y si ya está centrado no se ejecuta
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Boton pulsado", Toast.LENGTH_SHORT).show()
+        return false
+    }
+    // Muestra la latitud y la longitud
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "Estás aquí ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
     }
 }
